9ac10f9f3f1b0b79a07e5d980156953e1e1420c7
famgz <famgz@proton.me>
2023-09-24 15:38:22 -0300

    adding install dependencies

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # utils
 Set of generic Python utility tools that help with different personal projects.
+
+Usage
+-----
+
+```python
+from famgz_utils import <function>
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('src/famgz_utils/requirements.txt') as f:
+    REQUIREMENTS = f.readlines()
+
+setup(
+    name='famgz_utils',
+    version='0.1',
+    license='MIT',
+    author="famgz",
+    author_email='famgz@proton.me',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    # entry_points={
+        # 'console_scripts': [
+            # 'famgz_utils = famgz_utils.__main__:main'
+        # ]
+    # },
+    url='https://github.com/famgz/famgz_utils',
+    install_requires=REQUIREMENTS
+)
