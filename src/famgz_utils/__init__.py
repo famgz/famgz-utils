5164c46231262118435d477f36be60924b6c5774
famgz <famgz@proton.me>
2023-10-06 20:27:54 -0300

    mkv_tag export

--- src/famgz_utils/__init__.py
+++ src/famgz_utils/__init__.py
@@ -1,6 +1,6 @@
 from .config import print, input, rule, enable_print, disable_print
 from .cookies import Cookies
-from .downloader import downloader
+from .downloader import downloader, mkv_tag_path
 from .misc import *
 from .mouse_ import load_mouse_event
 from .time_ import *
